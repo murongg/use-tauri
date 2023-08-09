706ed106a6ddd5b50dd9c1259dbec27ccf611830
木荣 <372638156@qq.com>
2023-08-09 16:57:12 +0800

    chore: build

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -4,14 +4,12 @@ import { consola } from 'consola'
 import { version } from '../package.json'
 import { packages } from './constants'
 
-execSync('npm run build', { stdio: 'inherit' })
-
 let command = 'npm publish --access public'
 
 if (version.includes('beta'))
   command += ' --tag beta'
 
 for (const name of packages) {
-  execSync(command, { stdio: 'inherit', cwd: path.join('packages', name) })
+  execSync(command, { stdio: 'inherit', cwd: path.join('packages', name, 'dist') })
   consola.success(`Published @use-tauri/${name}`)
 }
--- scripts/release.ts
+++ scripts/release.ts
@@ -1,6 +1,10 @@
 import { execSync } from 'node:child_process'
-import { readJSONSync } from 'fs-extra'
+import path from 'node:path'
+import fs, { readJSONSync } from 'fs-extra'
+import { packages } from './constants'
 const { version: oldVersion } = readJSONSync('package.json')
+
+const rootDir = path.resolve(__dirname, '..')
 
 execSync('bumpp --no-commit --no-tag --no-push', { stdio: 'inherit' })
 
@@ -11,8 +15,23 @@ if (oldVersion === version) {
   process.exit()
 }
 
-execSync('npm run build', { stdio: 'inherit' })
-execSync('git add .', { stdio: 'inherit' })
+async function release() {
+  console.log(version)
+  for (const name of packages) {
+    const packageRoot = path.resolve(rootDir, 'packages', name)
+    const packageJSON = await fs.readJSON(path.join(packageRoot, 'package.json'))
+    for (const key of Object.keys(packageJSON.dependencies || {})) {
+      if (key.startsWith('@use-tauri/'))
+        packageJSON.version = version
+    }
+    await fs.writeJSON(path.join(packageRoot, 'package.json'), packageJSON, { spaces: 2 })
+  }
 
-execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
-execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
+  execSync('npm run build', { stdio: 'inherit' })
+  execSync('git add .', { stdio: 'inherit' })
+
+  execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
+  execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
+}
+
+release()
