87e702402c737adcc9fba408b49f68e03308e735
木荣 <372638156@qq.com>
2023-08-10 14:26:47 +0800

    feat: export subpackage types

--- packages/use-tauri/index.ts
+++ packages/use-tauri/index.ts
@@ -5,7 +5,17 @@ export {
   useWindowSize as useWindowSizeReact,
 } from '@use-tauri/react'
 
+export type {
+  UseWindowReturn as UseWindowReturnReact,
+  UseWindowSizeReturn as UseWindowSizeReturnReact,
+} from '@use-tauri/react'
+
 export {
   useWindow as useWindowVue,
   useWindowSize as useWindowSizeVue,
+} from '@use-tauri/vue'
+
+export type {
+  UseWindowReturn as UseWindowReturnVue,
+  UseWindowSizeReturn as UseWindowSizeReturnVue,
 } from '@use-tauri/vue'
